a88a5e3e567c6bb413670b23b050a1d7dfac4484
Quang Hiếu <dinhhieu9301@gmail.com>
2023-03-01 00:12:04 +0700

    add navigate

--- app/src/main/java/com/example/appandroid/MainActivity.java
+++ app/src/main/java/com/example/appandroid/MainActivity.java
@@ -1,17 +1,26 @@
 package com.example.appandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.GridLayout;
+import android.widget.LinearLayout;
 import android.widget.ViewFlipper;
 
 import com.example.appandroid.model.TypeOfFood;
 import com.example.appandroid.model.TypeOfFoodAdapter;
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
     Animation left_to_right,right_to_left;
     private RecyclerView recycleView_listTypeFood;
     private TypeOfFoodAdapter typeOfFoodAdapter;
+    LinearLayout linearLayout_account;
     private List<TypeOfFood> getList() {
         List<TypeOfFood> typeOfFoodList = new ArrayList<>();
         typeOfFoodList.add(new TypeOfFood(R.drawable.pizza, "Pizaa"));
@@ -37,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
         inital();
         actionViewFlipper();
         actionListTypeOfFood();
+        DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        NavigationView navigationView = findViewById(R.id.navigationView);
+        navigationView.setItemIconTintList(null);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupWithNavController(navigationView,navController);
     }
 
     private void inital() {
@@ -59,6 +81,8 @@ public class MainActivity extends AppCompatActivity {
         recycleView_listTypeFood.setLayoutManager(gridLayoutManager);
         recycleView_listTypeFood.setAdapter(typeOfFoodAdapter);
     }
+
+
 
 
 
